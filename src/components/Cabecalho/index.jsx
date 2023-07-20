d0fed76c854b514acdc5744cb0f1bb9836f6d1d5
Cléber Severo <clebersevero1998@gmail.com>
2023-07-20 14:40:18 -0300

    chore: desktop main logo inserted and styled

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -4,19 +4,22 @@ import styles from './Cabecalho.module.css'
 function Cabecalho() {
   return (
     <header className={styles.container}>
-        <nav className={styles.container__nav}>
+      <nav className={styles.container__nav}>
         <ul className={styles.list__menu}>
-                <li>Meteora</li>
-                <li>Home</li>
-                <li>Nossas Lojas</li>
-                <li>Novidades</li>
-                <li>Promoções</li>
-            </ul>
-            <ul className={styles.list}>
-                <li><input className={styles.search} type="text" placeholder='Digite o produto' /></li>
-                <li><button className={styles.btn}>Buscar</button></li>
-            </ul>
-        </nav>
+          <li >
+            <div className={styles.logo}></div>
+          </li>
+          <li>Meteora</li>
+          <li>Home</li>
+          <li>Nossas Lojas</li>
+          <li>Novidades</li>
+          <li>Promoções</li>
+        </ul>
+        <ul className={styles.list}>
+          <li><input className={styles.search} type="text" placeholder='Digite o produto' /></li>
+          <li><button className={styles.btn}>Buscar</button></li>
+        </ul>
+      </nav>
     </header>
   )
 }
