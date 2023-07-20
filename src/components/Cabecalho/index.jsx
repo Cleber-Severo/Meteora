e9033c59abc0702597379701412e5d2edba56358
Cléber Severo <clebersevero1998@gmail.com>
2023-07-20 10:04:40 -0300

    chore: further styling the header and reseting global css

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -3,16 +3,16 @@ import styles from './Cabecalho.module.css'
 
 function Cabecalho() {
   return (
-    <header>
-        <nav>
-            <ul>
+    <header className={styles.container}>
+        <nav className={styles.container__nav}>
+        <ul className={styles.list__menu}>
                 <li>Meteora</li>
                 <li>Home</li>
                 <li>Nossas Lojas</li>
                 <li>Novidades</li>
                 <li>Promoções</li>
             </ul>
-            <ul>
+            <ul className={styles.list}>
                 <li><input type="text" placeholder='Digite o produto' /></li>
                 <li><button>Buscar</button></li>
             </ul>
