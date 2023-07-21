9f3cd2e0e3d566d1eb9f8f6dc7270609f9190b74
Cléber Severo <clebersevero1998@gmail.com>
2023-07-21 14:30:44 -0300

    chore: mobile menu styiling, icons added

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import styles from './Cabecalho.module.css'
+import closeIcon from './marca-cruzada.png'
+import toggleIcon from './toggle-icon.png'
 
 function Cabecalho() {
   return (
     <header className={styles.container}>
       <nav className={styles.container__nav}>
-        <ul className={styles.list__menu}>
-          <li >
-            <div className={styles.logo}></div>
-          </li>
-          <li>Home</li>
-          <li>Nossas Lojas</li>
-          <li>Novidades</li>
-          <li>Promoções</li>
-        </ul>
+        <div className={styles.rightHeader}>
+          <div className={styles.logo}></div>
+          <ul className={styles.list__menu}>
+            <li><img src={closeIcon} alt="" /></li>
+            <li className={styles.item__menu}>Home</li>
+            <li className={styles.item__menu}>Nossas Lojas</li>
+            <li className={styles.item__menu}>Novidades</li>
+            <li className={styles.item__menu}>Promoções</li>
+          </ul>
+
+          <img src={toggleIcon} alt="" />
+
+        </div>
         <ul className={styles.list}>
           <li><input className={styles.search} type="text" placeholder='Digite o produto' /></li>
           <li><button className={styles.btn}>Buscar</button></li>
