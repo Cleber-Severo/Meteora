0c4fcaa0ee1ebae4f15e870d83420d7bf204339e
Cléber Severo <clebersevero1998@gmail.com>
2023-07-21 15:08:13 -0300

    chore: mobile menu fully responsive and animations added to the toggle event

--- src/components/Cabecalho/index.jsx
+++ src/components/Cabecalho/index.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Cabecalho.module.css'
 import closeIcon from './marca-cruzada.png'
 import toggleIcon from './toggle-icon.png'
 
 function Cabecalho() {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenuClass, setToggleMenuClass] = useState('closed');
+
+  function toggleMenuHandler(isOpened) {
+    console.log('here');
+    console.log(toggleMenuClass);
+    isOpened == true ? setToggleMenuClass('opened') : setToggleMenuClass('closed');
+  }
+
   return (
     <header className={styles.container}>
       <nav className={styles.container__nav}>
         <div className={styles.rightHeader}>
           <div className={styles.logo}></div>
-          <ul className={styles.list__menu}>
-            <li><img src={closeIcon} alt="" /></li>
+          <ul className={`${styles.list__menu} ${toggleMenu ? styles.opened : styles.closed}`}>
+            <li><img src={closeIcon} onClick={() => setToggleMenu(!toggleMenu) } alt="Close menu icon" /></li>
             <li className={styles.item__menu}>Home</li>
             <li className={styles.item__menu}>Nossas Lojas</li>
             <li className={styles.item__menu}>Novidades</li>
             <li className={styles.item__menu}>Promoções</li>
           </ul>
 
-          <img src={toggleIcon} alt="" />
+          <img src={toggleIcon} onClick={() => setToggleMenu(!toggleMenu) } alt="toggle menu icon" />
 
         </div>
         <ul className={styles.list}>
