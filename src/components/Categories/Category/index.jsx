e5a2464b305fa0f61ce437c5650ee0b3baac840d
Cléber Severo <clebersevero1998@gmail.com>
2023-07-25 15:51:01 -0300

    chore: mapping on data and styling the grid for different screen sizes

---
+++ src/components/Categories/Category/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Category.module.css'
+
+export const Category = ({screen , title, path }) => {
+    return (
+            <li className={`${styles[screen]}`}>
+                <img src={path} alt="" />
+                <h3>{title}</h3>
+            </li>
+    )
+}
--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import styles from './Categories.module.css'
+import categoryData from '../../data/categories.json'
+import { Category } from './Category';
 
 export const Categories = () => {
-  return (
-    <section>
-        <h1>Busque por categoria: </h1>
-        <ul>
-            <li>
-                  <img src="../../public/Images/Desktop/Categorias/Categoria Bolsa.png" alt="" />
-                <h3></h3>
-            </li>
-        </ul>
-    </section>
-  )
+    console.log(categoryData);
+    return (
+        <section>
+            <h1>Busque por categoria: </h1>
+
+            <ul className={styles.categoryList}>
+                {
+                    categoryData.map(item =>
+                        (<Category {...item} key={item.id} />))
+                }
+            </ul>
+        </section>
+    )
 }
