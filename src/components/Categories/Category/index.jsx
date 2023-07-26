0531ef6836e1c0590aa8114d79123c7a510af367
Cléber Severo <clebersevero1998@gmail.com>
2023-07-25 21:29:54 -0300

    fix: added lateral padding folllowing figma

--- src/components/Categories/Category/index.jsx
+++ src/components/Categories/Category/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './Category.module.css'
 
-export const Category = ({screen , title, path }) => {
+export const Category = ({ screen, title, path }) => {
     return (
-            <li className={`${styles[screen]} ${styles.category}`}>
-                <img src={path} alt="" />
-                <h3>{title}</h3>
-            </li>
+        <li className={`${styles[screen]} ${styles.category}`}>
+            <img src={path} alt="" />
+            <h3>{title}</h3>
+        </li>
     )
 }
