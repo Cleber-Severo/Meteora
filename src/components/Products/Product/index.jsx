c0e4bad4b9dc08284f0f2b595b15a76a34bd7728
Cléber Severo <clebersevero1998@gmail.com>
2023-07-27 17:34:49 -0300

    chore: princing iterated on cards from data

--- src/components/Products/Product/index.jsx
+++ src/components/Products/Product/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export const Product = ({path, title, description, screen}) => {
+export const Product = ({path, title, pricing, description, screen}) => {
   return (
     <article>
         <img src={path} alt="" />
         <div>
             <h3>{title}</h3>
             <p>{description}</p>
-            <h3>price</h3>
+            <h3>R$ {pricing}</h3>
             <button>Ver mais</button>
         </div>
     </article>
