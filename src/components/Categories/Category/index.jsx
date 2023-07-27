60c124e86ab849324ed2af4f6ada66e29adb0639
Cléber Severo <clebersevero1998@gmail.com>
2023-07-27 10:59:01 -0300

    fix:absolute path of categories data

--- src/components/Categories/Category/index.jsx
+++ src/components/Categories/Category/index.jsx
@@ -3,9 +3,10 @@ import styles from './Category.module.css'
 import testImage from './Categoria-camiseta.png'
 
 export const Category = ({ screen, title, path }) => {
+    const testImagejson = path;
     return (
         <li className={`${styles[screen]} ${styles.category}`}>
-            <img src={testImage} alt="" />
+            <img src={testImagejson} alt="" />
             <h3>{title}</h3>
         </li>
     )
