97e935a7987a43a7d1744a8083055a354eac52fb
Cléber Severo <clebersevero1998@gmail.com>
2023-07-28 10:26:53 -0300

    chore: facilities items styled more screens

--- src/components/Facilities/FacilitiesItems/index.jsx
+++ src/components/Facilities/FacilitiesItems/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './FacilitiesItems.module.css'
 
-export const FacilitiesItems = ({image, title, description}) => {
-  return (
-      <div className={styles.container}>
-        <img src={image} alt="" />
-        <div>
-            <h3>{title}</h3>
-            <p>{description}</p>
+export const FacilitiesItems = ({ image, title, description }) => {
+    return (
+        <div className={styles.container}>
+            <img src={image} alt="" />
+            <div>
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
