0c1c3695ea5c99bd9127d3bf9849148291270c2c
Cléber Severo <clebersevero1998@gmail.com>
2023-08-01 20:50:45 -0300

    chore: NewsLetter styled and button margin removed

--- src/components/NewsLetter/index.jsx
+++ src/components/NewsLetter/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import styles from './NewsLetter.module.css'
 import MeteoraBtn from '../MeteoraBtn'
+
 
 const NewsLetter = () => {
     return (
-        <form>
-            <p>Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? Cadastre-se!</p>
-            <div>
-                <input type="text" placeholder='Digite Seu email' />
-                <MeteoraBtn text='Enviar' />
-                
+        <form className={styles.container}>
+            <div className={styles.newsLetter}>
+                <p>Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? <b>Cadastre-se!</b></p>
+                <div>
+                    <input type="text" placeholder='Digite Seu email' />
+                    <MeteoraBtn text='Enviar' />
+                </div>
             </div>
         </form>
     )
