346e799edaf40153faa311a3e9e7da2d4d7185dc
Cléber Severo <clebersevero1998@gmail.com>
2023-08-01 20:23:42 -0300

    feat: NewsLetter component starting

---
+++ src/components/NewsLetter/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NewsLetter = () => {
+    return (
+        <form>
+            <p>Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? Cadastre-se!</p>
+            <div>
+                <input type="text" placeholder='Digite Seu email' />
+                <button>Enviar</button></div>
+        </form>
+    )
+}
+
+export default NewsLetter
