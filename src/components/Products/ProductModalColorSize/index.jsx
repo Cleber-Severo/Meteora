fe797710ec5d6505639c27323f776483bd6f520a
Cléber Severo <clebersevero1998@gmail.com>
2023-08-03 12:11:11 -0300

    chore: product modal radio container styled

--- src/components/Products/ProductModalColorSize/index.jsx
+++ src/components/Products/ProductModalColorSize/index.jsx
@@ -7,38 +7,47 @@ const ProductModalColorSize = () => {
       <div className={styles.radioContainer}>
         <h3>Cores:</h3>
         <div>
-          <input type="radio" name='colorName' id='lightblue' value='lightBlue'/>
-          <label htmlFor="lightblue">Azul Claro</label>
-
-          <input type="radio" name='colorName' id='offWhite' value='offWhite'/>
-          <label htmlFor="offWhite">Offwhite</label>
-          
-          <input type="radio" name='colorName' id='black' value='black'/>
-          <label htmlFor="black">Preto</label>
+          <div>
+            <input type="radio" name='colorName' id='lightblue' value='lightBlue' />
+            <label htmlFor="lightblue">Azul Claro</label>
+          </div>
+          <div>
+            <input type="radio" name='colorName' id='offWhite' value='offWhite' />
+            <label htmlFor="offWhite">Offwhite</label>
+          </div>
+          <div>
+            <input type="radio" name='colorName' id='black' value='black' />
+            <label htmlFor="black">Preto</label>
+          </div>
         </div>
       </div>
       <div className={styles.radioContainer}>
         <h3>Tamanhos:</h3>
         <div>
-          <input type="radio" name='sizesName' id='ppSize' value='ppSize'/>
-          <label htmlFor="ppSize">PP</label>
+          <div>
+            <input type="radio" name='sizesName' id='ppSize' value='ppSize' />
+            <label htmlFor="ppSize">PP</label>
+          </div>
 
-          <input type="radio" name='sizesName' id='pSize' value='pSize'/>
-          <label htmlFor="pSize">P</label>
-
-          <input type="radio" name='sizesName' id='mSize' value='mSize'/>
-          <label htmlFor="mSize">M</label>
-
-          <input type="radio" name='sizesName' id='gSize' value='gSize'/>
-          <label htmlFor="gSize">G</label>
-
-          <input type="radio" name='sizesName' id='ggSize' value='ggSize'/>
-          <label htmlFor="ggSize">GG</label>
-
-         
+          <div>
+            <input type="radio" name='sizesName' id='pSize' value='pSize' />
+            <label htmlFor="pSize">P</label>
+          </div>
+          <div>
+            <input type="radio" name='sizesName' id='mSize' value='mSize' />
+            <label htmlFor="mSize">M</label>
+          </div>
+          <div>
+            <input type="radio" name='sizesName' id='gSize' value='gSize' />
+            <label htmlFor="gSize">G</label>
+          </div>
+          <div>
+            <input type="radio" name='sizesName' id='ggSize' value='ggSize' />
+            <label htmlFor="ggSize">GG</label>
+          </div>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   )
 }
 
