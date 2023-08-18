2e38ca36dcaaec9da8a6c0948a6b730b6a9838d7
Cléber Severo <clebersevero1998@gmail.com>
2023-08-18 16:03:04 -0300

    fix: aside cart component created

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,5 @@
 
 import { useEffect, useState } from 'react'
-// import './App.css'
 import Footer from '/src/components/Footer'
 import NewsLetter from '/src/components/NewsLetter'
 import { Products } from '/src/components/Products'
@@ -10,17 +9,21 @@ import { Categories } from '/src/components/Categories'
 import { Facilities } from '../../components/Facilities'
 import { useFilterContext } from '../../context/Filter'
 
+import styles from './Home.module.css'
+import SideCart from '../../components/SideCart'
+
 function Home() {
     const { meteoraDB } = useFilterContext();
 
     if (meteoraDB) {
         return (
-            <div className='app-wrapper'>
+            <div className={styles.app_wrapper}>
                 <Banner />
                 <Categories/>
                 <Products />
                 <Facilities />
                 <NewsLetter />
+                <SideCart />
             </div>
         )
     }
